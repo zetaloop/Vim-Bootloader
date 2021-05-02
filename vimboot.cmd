5ba9b547dd6b87c9692734e5925b3e313a13ba67
Zetaspace <36418285+ZetaSp@users.noreply.github.com>
2021-05-03 01:34:42 +0800

    v1.2

--- vimboot.cmd
+++ vimboot.cmd
@@ -1,10 +1,11 @@
+:start
 @echo off
 cls
 title Vim Bootloader
 set vimbootanswer=111
 echo [Auto answer in 2 sec]
 echo.
-echo Options (Default[A]):
+echo Options (All Default[A] Restart[R]):
 echo UTF-8	--ON
 echo ~file	--ON
 echo =MORE=	--OFF
@@ -12,19 +13,23 @@ echo.
 
 :: choice Y:1 N:2
 
-choice /t 2 /d y /c yna /n /m "UTF-8 support ON?	[Y/n]"
+choice /t 2 /d y /c ynar /n /m "UTF-8 support ON?	[Y/n]"
 if %errorlevel%==1 set /a vimbootanswer=%vimbootanswer%+100
 if %errorlevel%==3 set vimbootanswer=221&goto :end
+if %errorlevel%==4 goto :start
 
-choice /t 2 /d y /c yna /n /m "KEEP .un~ .vim~ .swp?	[Y/n]"
+choice /t 2 /d y /c ynar /n /m "KEEP .un~ .vim~ .swp?	[Y/n]"
 if %errorlevel%==1 set /a vimbootanswer=%vimbootanswer%+10
 if %errorlevel%==3 set vimbootanswer=221&goto :end
+if %errorlevel%==4 goto :start
 
-choice /t 2 /d n /c yna /n /m "==== MORE ==== ?	[y/N]"
+choice /t 2 /d n /c ynar /n /m "==== MORE ==== ?	[y/N]"
 if %errorlevel%==1 set /a vimbootanswer=%vimbootanswer%+1
 if %errorlevel%==3 set vimbootanswer=221&goto :end
+if %errorlevel%==4 goto :start
 
 powershell exit
 ::That stupid pwsh was just for a suitable delay.
 :end
 exit %vimbootanswer%
+goto :EOF
