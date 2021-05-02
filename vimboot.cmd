62b66d2e0fc2ecd465fe440ac068a3650572276f
Zetaspace <36418285+ZetaSp@users.noreply.github.com>
2021-05-03 00:35:10 +0800

    v1.1

--- vimboot.cmd
+++ vimboot.cmd
@@ -13,14 +13,16 @@ echo.
 :: choice Y:1 N:2
 
 choice /t 2 /d y /c yna /n /m "UTF-8 support ON?	[Y/n]"
-if %errorlevel%==1 set /a vimbootanswer=%vimbootanswer%+1
-if %errorlevel%==3 set vimbootanswer=122&goto :end
-
-choice /t 2 /d y /n /m "KEEP .un~ .vim~ .swp?	[Y/n]"
-if %errorlevel%==1 set /a vimbootanswer=%vimbootanswer%+10
-
-choice /t 2 /d n /n /m "==== MORE ==== ?	[Y/n]"
 if %errorlevel%==1 set /a vimbootanswer=%vimbootanswer%+100
+if %errorlevel%==3 set vimbootanswer=221&goto :end
+
+choice /t 2 /d y /c yna /n /m "KEEP .un~ .vim~ .swp?	[Y/n]"
+if %errorlevel%==1 set /a vimbootanswer=%vimbootanswer%+10
+if %errorlevel%==3 set vimbootanswer=221&goto :end
+
+choice /t 2 /d n /c yna /n /m "==== MORE ==== ?	[y/N]"
+if %errorlevel%==1 set /a vimbootanswer=%vimbootanswer%+1
+if %errorlevel%==3 set vimbootanswer=221&goto :end
 
 powershell exit
 ::That stupid pwsh was just for a suitable delay.
