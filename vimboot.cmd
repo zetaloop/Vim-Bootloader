b4e3bd06d318de5bc9bfd5361d48d6063dc30ba2
Zetaspace <36418285+ZetaSp@users.noreply.github.com>
2021-05-30 03:05:04 +0800

    v1.3
    support custom default choice

--- vimboot.cmd
+++ vimboot.cmd
@@ -1,31 +1,57 @@
 :start
 @echo off
 cls
+
+
+set DefaultAnswer=211
+
+
 title Vim Bootloader
 set vimbootanswer=111
+set d1=%DefaultAnswer:~0,1%
+set d2=%DefaultAnswer:~1,1%
+set d3=%DefaultAnswer:~2,1%
 echo [Auto answer in 2 sec]
 echo.
 echo Options (All Default[A] Restart[R]):
-echo UTF-8	--ON
-echo ~file	--ON
-echo =MORE=	--OFF
+if %d1%==1 (
+	set c1=n
+	echo UTF-8	--N
+) else (
+	set c1=y
+	echo UTF-8	--Y
+)
+if %d2%==1 (
+	set c2=n
+	echo ~file	--N
+) else (
+	set c2=y
+	echo ~file	--Y
+)
+if %d3%==1 (
+	set c3=n
+	echo =MORE=	--N
+) else (
+	set c3=y
+	echo =MORE=	--Y
+)
 echo.
 
 :: choice Y:1 N:2
 
-choice /t 2 /d y /c ynar /n /m "UTF-8 support ON?	[Y/n]"
+choice /t 2 /d %c1% /c ynar /n /m "UTF-8 support ON?	[y/n]"
 if %errorlevel%==1 set /a vimbootanswer=%vimbootanswer%+100
-if %errorlevel%==3 set vimbootanswer=221&goto :end
+if %errorlevel%==3 set vimbootanswer=%DefaultAnswer%&goto :end
 if %errorlevel%==4 goto :start
 
-choice /t 2 /d y /c ynar /n /m "KEEP .un~ .vim~ .swp?	[Y/n]"
+choice /t 2 /d %c2% /c ynar /n /m "KEEP .un~ .vim~ .swp?	[y/n]"
 if %errorlevel%==1 set /a vimbootanswer=%vimbootanswer%+10
-if %errorlevel%==3 set vimbootanswer=221&goto :end
+if %errorlevel%==3 set vimbootanswer=%DefaultAnswer%&goto :end
 if %errorlevel%==4 goto :start
 
-choice /t 2 /d n /c ynar /n /m "==== MORE ==== ?	[y/N]"
+choice /t 2 /d %c3% /c ynar /n /m "==== MORE ==== ?	[y/n]"
 if %errorlevel%==1 set /a vimbootanswer=%vimbootanswer%+1
-if %errorlevel%==3 set vimbootanswer=221&goto :end
+if %errorlevel%==3 set vimbootanswer=%DefaultAnswer%&goto :end
 if %errorlevel%==4 goto :start
 
 powershell exit
