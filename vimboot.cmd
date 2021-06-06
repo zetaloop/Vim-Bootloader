7e8de41b687a047d71c2df4c65e8ff511946ceca
Zetaspace <36418285+ZetaSp@users.noreply.github.com>
2021-06-06 17:27:19 +0800

    v1.4.0
    support auto printscreen
    (a useless function :P

--- vimboot.cmd
+++ vimboot.cmd
@@ -1,19 +1,22 @@
 :start
 @echo off
-cls
-
+mode con cols=60 lines=24
 
 set DefaultAnswer=211
+:: 1=off, 2=on
 
+set DemoMode=2
+:: 1=off, 2=on
+:: Pause at the last choice; take a photo.
 
 title Vim Bootloader
 set vimbootanswer=111
 set d1=%DefaultAnswer:~0,1%
 set d2=%DefaultAnswer:~1,1%
 set d3=%DefaultAnswer:~2,1%
-echo [Auto answer in 1 sec]
+echo [Auto answer for 1 sec]
+echo Default[A/D] Reset[R] Close[X]:
 echo.
-echo Options (All Default[A/D] Reset[R]):
 if %d1%==1 (
 	set c1=n
 	echo UTF-8	--N
@@ -39,26 +42,38 @@ echo.
 
 :: choice Y:1 N:2
 
-choice /t 1 /d %c1% /c ynadr /n /m "UTF-8 support ON?	[y/n]"
+if %DemoMode%==2 (set t=0)else set t=1
+
+choice /t %t% /d %c1% /c ynadrx /n /m "UTF-8 support ON?	[y/n]"
 if %errorlevel%==1 set /a vimbootanswer=%vimbootanswer%+100
 if %errorlevel%==3 set vimbootanswer=%DefaultAnswer%&goto :end
 if %errorlevel%==4 set vimbootanswer=%DefaultAnswer%&goto :end
 if %errorlevel%==5 goto :start
+if %errorlevel%==6 goto :EOF
 
-choice /t 1 /d %c2% /c ynadr /n /m "KEEP .un~ .vim~ .swp?	[y/n]"
+choice /t %t% /d %c2% /c ynadrx /n /m "KEEP .un~ .vim~ .swp?	[y/n]"
 if %errorlevel%==1 set /a vimbootanswer=%vimbootanswer%+10
 if %errorlevel%==3 set vimbootanswer=%DefaultAnswer%&goto :end
 if %errorlevel%==4 set vimbootanswer=%DefaultAnswer%&goto :end
 if %errorlevel%==5 goto :start
+if %errorlevel%==6 goto :EOF
 
-choice /t 1 /d %c3% /c ynadr /n /m "==== MORE ==== ?	[y/n]"
+if %DemoMode%==2 (set t=2&set c3=x&call :prtsc&choice /t 1 /d n>nul)else set t=1
+
+choice /t %t% /d %c3% /c ynadrx /n /m "==== MORE ==== ?	[y/n]"
 if %errorlevel%==1 set /a vimbootanswer=%vimbootanswer%+1
 if %errorlevel%==3 set vimbootanswer=%DefaultAnswer%&goto :end
 if %errorlevel%==4 set vimbootanswer=%DefaultAnswer%&goto :end
 if %errorlevel%==5 goto :start
+if %errorlevel%==6 goto :EOF
 
 mshta vbscript:CreateObject("Wscript.Shell")(window.close)
 ::That stupid mshta was just for a suitable delay.
 :end
 exit %vimbootanswer%
+goto :EOF
+
+:prtsc
+start /min powershell Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.SendKeys]::SendWait('{PRTSC}');
+:: Call ShareX
 goto :EOF
