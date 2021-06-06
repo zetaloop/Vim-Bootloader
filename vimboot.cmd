cc305694a340cb9f6fa1c08a54f8f7a076917475
Zetaspace <36418285+ZetaSp@users.noreply.github.com>
2021-06-06 16:39:09 +0800

    v1.3.1
    powershell --> mshta

--- vimboot.cmd
+++ vimboot.cmd
@@ -11,7 +11,7 @@ set vimbootanswer=111
 set d1=%DefaultAnswer:~0,1%
 set d2=%DefaultAnswer:~1,1%
 set d3=%DefaultAnswer:~2,1%
-echo [Auto answer in 2 sec]
+echo [Auto answer in 1 sec]
 echo.
 echo Options (All Default[A] Restart[R]):
 if %d1%==1 (
@@ -39,23 +39,23 @@ echo.
 
 :: choice Y:1 N:2
 
-choice /t 2 /d %c1% /c ynar /n /m "UTF-8 support ON?	[y/n]"
+choice /t 1 /d %c1% /c ynar /n /m "UTF-8 support ON?	[y/n]"
 if %errorlevel%==1 set /a vimbootanswer=%vimbootanswer%+100
 if %errorlevel%==3 set vimbootanswer=%DefaultAnswer%&goto :end
 if %errorlevel%==4 goto :start
 
-choice /t 2 /d %c2% /c ynar /n /m "KEEP .un~ .vim~ .swp?	[y/n]"
+choice /t 1 /d %c2% /c ynar /n /m "KEEP .un~ .vim~ .swp?	[y/n]"
 if %errorlevel%==1 set /a vimbootanswer=%vimbootanswer%+10
 if %errorlevel%==3 set vimbootanswer=%DefaultAnswer%&goto :end
 if %errorlevel%==4 goto :start
 
-choice /t 2 /d %c3% /c ynar /n /m "==== MORE ==== ?	[y/n]"
+choice /t 1 /d %c3% /c ynar /n /m "==== MORE ==== ?	[y/n]"
 if %errorlevel%==1 set /a vimbootanswer=%vimbootanswer%+1
 if %errorlevel%==3 set vimbootanswer=%DefaultAnswer%&goto :end
 if %errorlevel%==4 goto :start
 
-powershell exit
-::That stupid pwsh was just for a suitable delay.
+mshta vbscript:CreateObject("Wscript.Shell")(window.close)
+::That stupid mshta was just for a suitable delay.
 :end
 exit %vimbootanswer%
 goto :EOF
